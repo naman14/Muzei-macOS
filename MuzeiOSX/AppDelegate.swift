d035d1223c1d30e8b71ffef8f1843fdbdf69fa1f
naman14 <namandwivedi14@gmail.com>
2016-12-16 20:14:47 +0530

    initial commit

---
+++ MuzeiOSX/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MuzeiOSX
+//
+//  Created by Naman on 16/12/16.
+//  Copyright © 2016 naman14. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
