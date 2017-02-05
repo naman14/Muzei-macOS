ca4aa6af41e00f9d17d2c89d1f2967b4bbd1fc32
naman14 <namandwivedi14@gmail.com>
2017-02-06 02:58:43 +0530

    reddit api

--- MuzeiOSX/RedditSource.swift
+++ MuzeiOSX/RedditSource.swift
@@ -7,14 +7,66 @@
 //
 
 import Foundation
+import SwiftyJSON
+import Alamofire
+import Kingfisher
 
 class RedditSource: WPSourceProtocol {
     
+    let BASE_URL = "https://www.reddit.com/r/earthporn/top.json?t=day&limit=10"
+    
     func getWallpaper(callback: @escaping (URL) -> Void, failure: @escaping () -> Void) {
         
-        let image = "/Users/naman/Desktop/20161218-netherlandish-proverbs-pieter-bruegel-the-elder.jpg"
-        callback(URL(fileURLWithPath: image))
+        Alamofire.request(BASE_URL).responseJSON { (response) in
+            
+            if let responseStr = response.result.value {
+                let json = JSON(responseStr)
+                
+                let children = json["data"]["children"].arrayValue
+                
+                let data = children[0]["data"]
+                
+                let title = data["title"].stringValue
+                
+                let source = data["preview"]["images"][0]["source"]
+                
+                let imageUri = source["url"].stringValue
+                let width = source["width"].intValue
+                let height = source["height"].intValue
+                
+                ImageDownloader.default.downloadImage(with: URL(string: imageUri)!) {
+                    (image, error, url, data) in
+                    
+                    let processedImage = WPProcessor().processImage(originalImage: image!)
+                    
+                    if (error != nil) {
+                        failure()
+                        print(error?.description ?? "Error occurred")
+                        return
+                    }
+                    
+                    let fullURL = WPProcessor().getWallpaperFileUrl(
+                        fileName: (imageUri as NSString).lastPathComponent as NSString)
+                    print(fullURL)
+                    if WPProcessor().imageToFile(image: processedImage, imageURL: fullURL!, ext:(imageUri as NSString).pathExtension) {
+                        callback(fullURL!)
+                    } else {
+                        failure()
+                        print("Error occurred2")
+                        
+                    }
+                    
+                }
+                
+            }
+        }
+        
+        
     }
-
 }
+
+
+
+
+
 
